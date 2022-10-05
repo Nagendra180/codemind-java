61d55ecfdbdfb079586838673b3f70630d96dd46
Nagendra180 <106660151+Nagendra180@users.noreply.github.com>
2022-10-05 18:09:30 +0530

    import java.util.Scanner; class area {     public static void main(String args[])     {         int a,b,c;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();         c=sc.nextInt();         System.out.println(2*a*b*c*512);     } }

---
+++ Disk_Capacity.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class area {
+    public static void main(String args[])
+    {
+        int a,b,c;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=sc.nextInt();
+        System.out.println(2*a*b*c*512);
+    }
+}

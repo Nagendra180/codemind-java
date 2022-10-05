e3aeebb9b6b1ed90d122cafb0552ba49bbbe4d45
Nagendra180 <106660151+Nagendra180@users.noreply.github.com>
2022-10-05 17:58:27 +0530

    import java.util.Scanner; class area {     public static void main(String args[])     {         String  a;         Scanner sc=new Scanner(System.in);         a=sc.nextLine();         System.out.println(a);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class area {
+    public static void main(String args[])
+    {
+        String  a;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextLine();
+        System.out.println(a);
+    }
+}
